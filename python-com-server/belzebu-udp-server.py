7a92787c2191fc4a46b4a9d77cb1bbf02c438403
Ekironji <giovanni.burresi@aidilab.it>
2015-04-10 14:51:07 +0000

    basic version work, no message controll, total forwarding to SAM3x

--- python-com-server/belzebu-udp-server.py
+++ python-com-server/belzebu-udp-server.py
@@ -2,6 +2,9 @@ import socket
 import serial
 import struct
 import fcntl
+import json                                 
+
+port = 9005
 
 try:
     UDPSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
@@ -11,32 +14,36 @@ except socket.error, msg:
     sys.exit()    
 
 try:
-    UDPSock.bind(("", 9002))
+    UDPSock.bind(("", port))
+    print 'Socket created on port ' + str(port)
 except socket.error, msg:
-    print 'Bind failed. Error code: ' +    str(msg[0]) + ' - Message: ' + msg[1]
+    print 'Bind failed. Error code: ' +  str(msg[0]) + ' - Message: ' + msg[1]
     
 print 'Socket bind complete'
 
-ser = serial.Serial('/dev/ttymxc3',9600,timeout=1)
+ser = serial.Serial('/dev/ttymxc3',115200,timeout=1)
 ser.flushOutput()
 
-ip_this = socket.inet_ntoa(fcntl.ioctl(UDPSock.fileno(),0x8915, struct.pack('256s',"wlan0"[:15]))[20:24])
-print 'IP address of this server: ' + ip_this
+# get local ip address on selected interface -----------------------------------------+
+#                                                                                     |
+#                                                                                     |
+#                                                                                     V
+ip_this = socket.inet_ntoa(fcntl.ioctl(UDPSock.fileno(),0x8915, struct.pack('256s',"eth0"[:15]))[20:24])
+print 'IP address of belzebu server: ' + ip_this
 
-from subprocess import call
-call(["set_alpha", "fb0", "0"])
 
+# insert a controll 
 while True:
-    data, addr = UDPSock.recvfrom(4)
-    print data, ' ', addr[0], '  bytes: ', ord(data[3]), ' ', ord(data[2]), ' ', ord(data[1]), ' ', ord(data[0])
+    data, addr = UDPSock.recvfrom(128)
+    print addr[0], ' say ', data,  '   bytes: ', ord(data[0])
+
     if data == "ciao":
-        UDPSock.sendto(ip_this, addr )
-    elif ord(data[3]) == 208:
-		fileToPlay = "uri=file:///home/ubuntu/videos/" + str(ord(data[0])) + ".mp4";
-		from subprocess import call
-		call(["gst-launch-0.10", "playbin2", fileToPlay])
+        UDPSock.sendto(ip_this, addr)
     else:    
         ser.write(data)
 	ser.flushOutput()
     
 UDPSock.close()    
+
+
+# pert il python e json https://docs.python.org/2/library/json.html
